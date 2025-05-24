eec4350cf08782111524150c2cc43f2d867bad9a
Bernard Chua <bernardcks123@gmail.com>
2025-05-24 17:51:35 +0800

    Completed exercise 1.1-1.2, finished part 1a

---
+++ fullstackopen-course/part1/courseinfo/src/App.jsx
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+export const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.p1} exercises={props.ex1}/>
+      <Part part={props.p2} exercises={props.ex2}/>
+      <Part part={props.p3} exercises={props.ex3}/>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Numer of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content p1={part1} ex1={exercises1} p2={part2} ex2={exercises2} p3={part3} ex3={exercises3}/>
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+    </div>
+  )
+}
+
+export default App
